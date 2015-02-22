767a2e7b5e67754a3a103358593f5a755fd70fb0
Melanie Loff-Bird <melanie@unison.net>
2015-02-21 19:43:32 -0500

    added imagery for homepage, buttons and icons

--- js/app.js
+++ js/app.js
@@ -4,44 +4,65 @@ $(document).foundation();
 
 
 // MENU NAVIGATION
-(function(){
+// (function(){
 
-	var button = document.getElementbyId('cn-button'),
-    wrapper = document.getElementById('cn-wrapper');
+// 	var button = document.getElementbyId('cn-button'),
+//     wrapper = document.getElementById('cn-wrapper');
 
-	//open and close menu when the button is clicked
-	var open = false;
-	toggle.addEventListener('click', handler, false);
-	wrapper.addEventListener('click', cnhandle, false);
+// 	//open and close menu when the button is clicked
+// 	var open = false;
+// 	toggle.addEventListener('click', handler, false);
+// 	wrapper.addEventListener('click', cnhandle, false);
 
-	function cnhandle(e){
-		e.stopPropagation();
-	}
+// 	function cnhandle(e){
+// 		e.stopPropagation();
+// 	}
 
-	function handler(e){
-		if (!e) var e = window.event;
-	 	e.stopPropagation();//so that it doesn't trigger click event on document
+// 	function handler(e){
+// 		if (!e) var e = window.event;
+// 	 	e.stopPropagation();//so that it doesn't trigger click event on document
 
-	  	if(!open){
-	    	openNav();
-	  	}
-	 	else{
-	    	closeNav();
-	  	}
-	}
-	function openNav(){
-		open = true;
-		button.innerHTML = "-";
-	    classie.add(wrapper, 'opened-nav');
-	}
-	function closeNav(){
-		open = false;
-		button.innerHTML = "+";
-		classie.remove(wrapper, 'opened-nav');
-	}
-	document.addEventListener('click', closeNav);
+// 	  	if(!open){
+// 	    	openNav();
+// 	  	}
+// 	 	else{
+// 	    	closeNav();
+// 	  	}
+// 	}
+// 	function openNav(){
+// 		open = true;
+// 		button.innerHTML = "-";
+// 	    classie.add(wrapper, 'opened-nav');
+// 	}
+// 	function closeNav(){
+// 		open = false;
+// 		button.innerHTML = "+";
+// 		classie.remove(wrapper, 'opened-nav');
+// 	}
+// 	document.addEventListener('click', closeNav);
 
-})();
+// })();
+
+
+
+// FLOORPLAN BUTTONS
+
+$(".focusoverlay").hide();
+$(".collaboverlay").hide();
+$(".playoverlay").hide();
+
+$(".f-map").click(function(){
+    $(".focusoverlay").toggle();
+});
+
+$(".c-map").click(function(){
+    $(".collaboverlay").toggle();
+});
+
+$(".p-map").click(function(){
+    $(".playoverlay").toggle();
+});
+
 
 
 
